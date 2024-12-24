a728751ffb459a3ceb95b8dc04fe229782c816e7
parisapaki <parisapakii@gmail.com>
2024-12-24 22:47:45 +0330

    create 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,21 @@ import About from "./pages/about/About";
 import { Route,Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+import CreateArticle from "./pages/createArticle/CreateArticle";
+import NotFound from "./pages/notfound/NotFound";
 
 export default function App() {
   return (
   <div className="App rtl">
     <Navbar />
     <Routes>
-      <Route  path="/" element={<Home />}/>
-      <Route  path="newarticle" element={<NewArticle />}/>
-      <Route  path="articles" element={<Articles />}/>
-      <Route  path="about" element={<About />}/>
+      <Route path="*" element={<NotFound />} />
+      <Route path="/" element={<Home />}/>
+      <Route path="newarticle" element={<NewArticle />}/>
+      <Route path="articles" element={<Articles />}/>
+      <Route path="about" element={<About />}/>
       <Route path="articles/:id" element={<Articles />} />
+      <Route path="createArticle" element={<CreateArticle />} />
     </Routes>
     <Footer />
     
---
+++ src/pages/notfound/NotFound.jsx
@@ -0,0 +1,10 @@
+export default function NotFound(){
+    return(
+        <div class="bg-gray-100">
+            <div class="h-screen flex flex-col justify-center items-center">
+            <h1 class="text-8xl font-bold text-gray-800">404</h1>
+            <p class="text-4xl font-medium text-gray-800">Page Not Found</p>
+            </div>
+        </div>
+    )
+}
